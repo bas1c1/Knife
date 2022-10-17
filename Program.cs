cbd77b98a0ddc2c309b5900f5c7f8007cf3d3574
bas1c <68533770+bas1c1@users.noreply.github.com>
2022-10-17 22:16:44 +0300

    Knife

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+
+public class Knife
+{
+    HttpListener listener = new HttpListener();
+
+    public void _listener(string responseString, string s)//, params string[] prefixes)
+    {
+        if (!HttpListener.IsSupported)
+        {
+            Console.WriteLine("Your computer doesn't support Knife");
+            return;
+        }
+
+        listener.Prefixes.Add(s);
+
+        listener.Start();
+
+        HttpListenerContext context = listener.GetContext();
+        HttpListenerResponse response = context.Response;
+
+        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+        response.ContentLength64 = buffer.Length;
+
+        System.IO.Stream output = response.OutputStream;
+        output.Write(buffer,0,buffer.Length);
+
+        output.Close();
+        listener.Stop();
+    }
+
+    public void stopListener()
+    {
+        listener.Stop();
+    }
+}
